7c006c83e58fc496277090ad40ed884361811446
Danindu-Seniya <danindunawa@gmail.com>
2024-07-17 01:06:01 +0530

    Create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+//components
+import Header from "@/components/Header";
+
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -21,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        {children}
+        </body>
     </html>
   );
 }
