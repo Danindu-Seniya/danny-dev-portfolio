c1bc7bce9faf2950ae9b2d753adef9c6d070a1da
Danindu-Seniya <danindunawa@gmail.com>
2024-07-28 23:53:03 +0530

    feat: contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,21 @@
-
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 ">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-5xl text-accent">Let&rsquo;s Work Together!</span>
+            <p className="max-w-[600px] mb-9 text-white/80 text-xl py-10">
+            I am looking to be part of a team that is committed to making a positive impact through technology. With my background in full-stack development, I am ready to help build solutions that enhance people&rsquo;s lives.
+            </p>
+            <p>Email: danindunawa@gmail.com</p>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
